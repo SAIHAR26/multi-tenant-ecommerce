30110e55f32d89f130a8a3276e06794df1ee36b0
Pranathi <pranathidaneswaripasupuleti@gmail.com>
2026-05-13 22:12:19 +0530

    Improved MongoDB schemas and relationships

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+
 const orderSchema = new mongoose.Schema({
 userId: {
 type: mongoose.Schema.Types.ObjectId,
 ref: "User",
+required: true,
 },
 totalAmount: {
 type: Number,
+required: true,
 },
 status: {
 type: String,
@@ -14,6 +17,10 @@ default: "PROCESSING",
 },
 deliveryAddress: {
 type: String,
+required: true,
 },
+}, {
+timestamps: true
 });
+
 module.exports = mongoose.model("Order", orderSchema);
